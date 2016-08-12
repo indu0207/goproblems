02afcadee4030574c1b8741954432c9df2e4b53d
Indumathy Rajagopalan <indu.rajagopalan@gmail.com>
2016-08-11 23:28:28 -0700

    p1

---
+++ simplearrayrearrange.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	B := [10]int{2, 1, 9, 6, 5, 4, 5, 8, 3, 10}
+
+	fmt.Printf("Original array is %v \n", B)
+
+	index := 3
+
+	start := 0
+	end := len(B) - 1
+
+	for start < end {
+
+		if B[start] < B[index] {
+			start++
+
+		}
+		if B[end] > B[index] {
+			end--
+		}
+		fmt.Printf("The start and end are %d,%d \n", B[start], B[end])
+		if B[start] > B[end] {
+			temp := B[start]
+			B[start] = B[end]
+			B[end] = temp
+		}
+
+	}
+	fmt.Printf("Swapped array is %v \n", B)
+}
